852abce37813e45225890184a23eafb1ff61130b
BenjaminCoajou <coajou.benjamin@gmail.com>
2021-06-05 23:10:42 +0200

    html and css

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Header from '../Header';
 import Home from '../Home';
+import Country from '../Country';
 
 const App = () => {
 
   return (
     <div>
       <Header/>
-      <Home/>
+      <Country/>
     </div>
   );
 }
--- src/components/Country/index.js
+++ src/components/Country/index.js
@@ -3,9 +3,42 @@ import './styles.css';
 const Country = () => {
 
     return (
-        <div>
-            Country
-        </div>
+        <main>
+            <a className="back" href="#">
+                <span class="material-icons">
+                    arrow_back
+            </span>
+            Back
+            </a>
+            <div className="card-country">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Flag_of_France.svg/langfr-225px-Flag_of_France.svg.png" className="card-img-country" alt="..." />
+                <div className="card-body-country">
+                    <h2>Country</h2>
+                    <div className="card-part">
+                        <div className="card-part-1">
+                            <p>Native :</p>
+                            <p>Population :</p>
+                            <p>Region :</p>
+                            <p>Sub Region :</p>
+                            <p>Capital :</p>
+                        </div>
+                        <div className="card-part-2">
+                            <p>Top Level Domain :</p>
+                            <p>Currencies :</p>
+                            <p>Languages :</p>
+                        </div>
+                    </div>
+                    <div className="card-bottom-country">
+                        <p>Border Countries :</p>
+                        <ul className="card-bottom-list">                            
+                            <li>country 1</li>
+                            <li>country 1</li>
+                        </ul>
+                    </div>
+
+                </div>
+            </div>
+        </main>
     );
 }
 
