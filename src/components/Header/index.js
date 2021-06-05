87f8b43c59c012fcf014b304ccccc9feb772a399
BenjaminCoajou <coajou.benjamin@gmail.com>
2021-06-05 12:36:39 +0200

    html and css

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,9 +3,15 @@ import './styles.css';
 const Header = () => {
 
     return (
-        <div>
-            Header
-        </div>
+        <header>
+            <h1>Where in the world ?</h1>
+            <button type="button" className="dark-mode">
+                <span className="material-icons">
+                    dark_mode
+                </span>
+                <p>Darck Mode</p>
+            </button>
+        </header>
     );
 }
 
